c4d547804417deb820d8811494f45d85a8ed0556
Deylian Heykoop <deylian@heykoop.nl>
2024-06-13 15:53:13 +0200

    Added hotwire (ig)

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,8 @@
 class HomepageController < ApplicationController
   def index
+    @birthdate = Date.new(2009, 8, 10)
+    @age = Date.today.year - @birthdate.year
+    @age -= 1 if Date.today.month < @birthdate.month || (Date.today.month == @birthdate.month && Date.today.day < @birthdate.day)
+    @age
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// application.js
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+import animateController from "../controllers/animate_controller"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
